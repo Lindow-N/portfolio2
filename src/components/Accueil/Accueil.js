235ea7921d64f1cf8a62b770a9a3762378bc7438
Lindow-N <anthonycasaresit@gmail.com>
2021-12-02 08:50:07 +0100

    mobilep1

---
+++ src/components/Accueil/Accueil.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import style from './Accueil.module.css';
+
+function Accueil() {
+  return (
+  <>  
+
+<div className={style.DivHeader2}>
+<h3 className={style.H3Header}>PORTFOLIO</h3>
+</div>
+
+       <div className={style.DivHeader}>
+          
+
+          <div className={style.DivHeader3}>
+          
+            <h1 className={style.H1Header}>ANTHONY CASARES</h1>
+            <h2 className={style.H2Header}>Développeur Web – Full Stack JavaScript</h2>
+          
+          </div>
+            
+          
+          </div>
+
+
+      
+  </>  
+  );
+}
+
+export default Accueil;
