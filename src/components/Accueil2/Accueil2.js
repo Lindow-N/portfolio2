bf89965753c9f190f72afb4b430e18101a26e1dd
Lindow-N <anthonycasaresit@gmail.com>
2021-12-03 10:46:49 +0100

    ok

--- src/components/Accueil2/Accueil2.js
+++ src/components/Accueil2/Accueil2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './Accueil2.module.css';
 
 import Jap from './imgApp/Jap.PNG';
-import tesla from './imgApp/Tesla.PNG';
+import tesla from './imgApp/onrec.PNG';
 import Notfound from './imgApp/404.PNG';
 import Tinder from './imgApp/Tinder.PNG';
 
@@ -12,27 +12,37 @@ function Accueil2() {
   <>  
 
 <div className={style.Flexbox1}>
-    <div className={style.Flexbox2}>
+  <div className={style.Flexbox2}>
+    <a href="https://lindow-n.github.io/projet-Learn-Japan/" className={style.FlexboxLink}>
       <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={Jap} alt=""/></div>
       <h4 className={style.H4flexbox}>Japanese Courses Page</h4>
-    </div>
-
-    <div className={style.Flexbox2}>
-    <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={tesla} alt=""/></div>
-      <h4 className={style.H4flexbox}>Site Tesla</h4>
-    </div>
+    </a>
+  </div>
+  
+  <div className={style.Flexbox2}>
+    <a href="https://lindow-n.github.io/onrecPortfolio/#/" className={style.FlexboxLink}>
+      <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={tesla} alt=""/></div>
+      <h4 className={style.H4flexbox}>Site Onrec</h4>
+    </a>
+  </div>
 </div>  
 
 <div className={style.Flexbox1}>
     <div className={style.Flexbox2}>
-    <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={Notfound} alt=""/></div>
+    <a href="https://Lindow-N.github.io/Page404-devChallenges.io" className={style.FlexboxLink}>
+      <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={Notfound} alt=""/></div>
       <h4 className={style.H4flexbox}>Page 404 - Dev challenge</h4>
+      </a>
     </div>
 
     <div className={style.Flexbox2}>
+    <a href="https://lindow-n.github.io/tinder/" className={style.FlexboxLink}>
+
     <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={Tinder} alt=""/></div>
       <h4 className={style.H4flexbox}>Site tinder</h4>
+      </a>
     </div>
+    
 </div>
   
   </>  
