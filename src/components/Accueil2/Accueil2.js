634fd3c35a4f1198ab0586d29423313eef15d244
Lindow-N <anthonycasaresit@gmail.com>
2022-01-11 16:57:15 +0100

    ok

--- src/components/Accueil2/Accueil2.js
+++ src/components/Accueil2/Accueil2.js
@@ -6,6 +6,7 @@ import tesla from './imgApp/onrec.PNG';
 import Notfound from './imgApp/404.PNG';
 import Tinder from './imgApp/Tinder.PNG';
 import airbnb from './imgApp/Capture (2).PNG';
+import mk from './imgApp/Captaaaaaaaaaaaaaaure.PNG';
 
 function Accueil2() {
   return (
@@ -28,10 +29,13 @@ function Accueil2() {
 </div>  
 
 <div className={style.Flexbox1}>
+  
+
     <div className={style.Flexbox2}>
-    <a href="https://Lindow-N.github.io/Page404-devChallenges.io" className={style.FlexboxLink}>
-      <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={Notfound} alt=""/></div>
-      <h4 className={style.H4flexbox}>Page 404 - Dev challenge</h4>
+    <a href="https://lindow-n.github.io/task2/" className={style.FlexboxLink}>
+
+    <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={mk} alt=""/></div>
+      <h4 className={style.H4flexbox}>Markdown Convertisseur</h4>
       </a>
     </div>
 
@@ -53,6 +57,15 @@ function Accueil2() {
       <h4 className={style.H4flexbox}>Site airbnb</h4>
       </a>
     </div>
+
+    <div className={style.Flexbox2}>
+    <a href="https://Lindow-N.github.io/Page404-devChallenges.io" className={style.FlexboxLink}>
+      <div className={style.Flexboximgdiv}><img className={style.Flexboximg} src={Notfound} alt=""/></div>
+      <h4 className={style.H4flexbox}>Page 404 - Dev challenge</h4>
+      </a>
+    </div>
+    
+
     </div>
   </>  
   );
